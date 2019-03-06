73def85261df4e65e1c0dce7527aeeba8d46275c
Yong Hao TENG <yonghao.teng@gmail.com>
2019-03-06 12:07:42 +0800

    Add files

--- A485/A485-setup.sh
+++ A485/A485-setup.sh
@@ -1,22 +1,39 @@
 
-# Disable bell
-echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
+# Update everything first
+sudo pacman -Syu --noconfirm
 
-# Setup polybar
-mkdir -p $HOME/.config/polybar/
+# Generate keys
+ssh-keygen -t ecdsa -b 521 -q -N "" -f ~/.ssh/id_ecdsa
 
 # Setup NTP
 sudo pacman -S ntp
 sudo timedatectl set-ntp true
 
+# Disable BT
+sudo rfkill block bluetooth
 
-install_pulse
+# Install programs
+sudo pacman -S polybar
 
+# Install yay
+git clone https://aur.archlinux.org/yay.git /var/tmp
+cd /var/tmp
+makepkg -si
 
-echo "defaults.pcm.card 1" >> /etc/asound.conf
-defaults.ctl.card 1
+# Setup NTP
+sudo pacman -S ntp
+sudo timedatectl set-ntp true
 
 # Disable BT
 sudo rfkill block bluetooth
 
+# Install AUR packages
+yay -S google-chrome
+yay -S ttf-material-design-icons-webfont
+
+# Uninstall default programs that came with Manjaro i3
+sudo pacman -R palemoon-bin
+
+# List unused orphans packages
+pacman -Qdtm
 
