aefa991c1ecf9acf04b74d589e4c6fdccb3a23e9
Yong Hao TENG <yonghao.teng@gmail.com>
2019-03-15 01:22:19 +0800

    Add files

--- sync.sh
+++ sync.sh
@@ -5,16 +5,14 @@
 cp -r ~/.config/i3              .config/
 cp -r ~/.config/polybar         .config/
 cp -r ~/.config/rofi            .config/
+cp -r ~/.config/dunst           .config/
 cp -r ~/.config/.Xresources.d   .config/
-
 cp -r ~/.config/ranger          .config/
 cp -r ~/.config/nvim            .config/
 cp -r ~/.config/scripts         .config/
 
 cp ~/.config/cmus/rc            .config/cmus/
 cp ~/.config/mimeapps.list      .config/
-
-
 
 cp -r ~/.screenlayout           .
 cp ~/.zshrc                     .
